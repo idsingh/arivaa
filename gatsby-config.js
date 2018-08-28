9b65dba60d60d7ffdbf116f8fa8527f6158d8711
Inderdeep Singh <inderdeep.singh@laxaar.com>
2018-08-28 20:49:42 +0530

    Antd Integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,11 @@ module.exports = {
       },
     },'gatsby-plugin-react-helmet',
     `gatsby-transformer-remark`,
+      {
+          resolve: 'gatsby-plugin-babel',
+          options: {
+              plugins: ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }]
+          }
+      }
 ]
 }
