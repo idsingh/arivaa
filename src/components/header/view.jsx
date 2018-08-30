828686060c683d0ff24affcd15b42c49d6e0de39
Jasjot <jasjot.singh30@gmail.com>
2018-08-30 12:12:55 +0530

    fixes

--- src/components/header/view.jsx
+++ src/components/header/view.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './styles.less'
 import Link from 'gatsby-link'
+import { Layout, Menu } from 'antd'
 
+const {Header} = Layout
 /**
  * Returns the JSX Markup for the view
  * @returns {XML}
@@ -9,13 +11,14 @@ import Link from 'gatsby-link'
 var view = function () {
     const {siteTitle} = this.props
     return (
-        <div className="header">
-            <div>
-                <h3>
-                    <Link to="/">{siteTitle}</Link>
-                </h3>
-            </div>
-        </div>
+        <Header className="header">
+            <div className="logo"/>
+            <Menu mode="horizontal" defaultSelectedKeys={['2']} style={{lineHeight: '64px'}}>
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+        </Header>
     )
 }
 export default view
